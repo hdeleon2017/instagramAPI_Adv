7b82a6a52d7eef3a7389372b8c57bc4a39d698fd
hdeleon2017 <hdeleon@cristoreyny.org>
2016-02-04 13:49:12 +0000

    flickr api project

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,17 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "b3aca93577762a361363318839281b22";
+var userInput = "";
 
 $(document).ready(function() {
-    // your code here
+    $("#search-btn").click(function() {
+        userInput = $("#search-query").val();
+        searchFlickr(clientID, userInput, 10, function(photos) {
+            $('#search-results').empty('#search-results');
+            for (var i = 0; i < photos.photo.length; i++)
+                $('#search-results').append('<img src=' + photos.photo[i].url_m + '>');
+        });
+    });
+    $("#reset-btn").click(function() {
+        $('#search-results').empty('#search-results');
+    });
 });
